35123e33964e064c1cca2d3cbe52274a77c34c9f
Nathan Schaefer <nkschaef@ucsc.edu>
2019-04-22 14:54:35 -0700

    Added option to select clades containing members of specific populations in dump_haplens

--- src/dump_haplens.cpp
+++ src/dump_haplens.cpp
@@ -21,11 +21,11 @@ using namespace std;
 void print_haplens(treeNode* tree, int& traversal_index, string& chrom, 
     long int pos, long int minsize, long int maxsize, long int minlen, bool freqs_given,
     map<pair<long double, long double>, vector<long double> >& ages_freqs, float p_cutoff,
-    float tmrca_cutoff){
+    float tmrca_cutoff, bool use_selected_haps, cladeset& selected_haps){
     if (tree->parent != NULL && tree->persistence > 0){
         cladeset st = tree->subtree_leaves();
         if (st.count() >= minsize && tree->persistence >= minlen && (maxsize == -1 ||
-            st.count() <= maxsize)){
+            st.count() <= maxsize) && (!use_selected_haps || (st & selected_haps).count() > 0)){
             long double tmrca = tree->dist_below/(tree->dist_below+tree->dist_above);
             if (tree->dist_below + tree->dist_above == 0){
                 tmrca = 0;
@@ -73,7 +73,7 @@ void print_haplens(treeNode* tree, int& traversal_index, string& chrom,
     for (vector<treeNode*>::iterator child = tree->children.begin(); child !=
         tree->children.end(); ++child){
         print_haplens(*child, traversal_index, chrom, pos, minsize, maxsize, minlen, freqs_given, 
-            ages_freqs, p_cutoff, tmrca_cutoff);
+            ages_freqs, p_cutoff, tmrca_cutoff, use_selected_haps, selected_haps);
     }
 }
 
@@ -144,10 +144,17 @@ to contain in order to be printed (must be < num hapotypes -1)\n");
 persist in order to be printed (must be >= 1)\n");
     fprintf(stderr, "   --freqs -f (OPTIONAL) to calculate probabilities of clade sizes \
 based on TMRCA, provide table computed by utilities/calc_ages_freqs.py\n");
-    fprintf(stderr, "   --p_cutoff -p (OPTIONAL) if providing a frequency table (-f argument), \
+    fprintf(stderr, "   --p_cutoff -P (OPTIONAL) if providing a frequency table (-f argument), \
 the maximum p-value allowed to print a clade\n");
     fprintf(stderr, "   --tmrca_cutoff, -t (OPTIONAL) a TMRCA value (as percent of total divergence \
 to outgroup) above which not to print clades\n");
+    fprintf(stderr, "   --indvs -v (OPTIONAL) A file listing haplotype names - required \
+if using -p and -i options\n");
+    fprintf(stderr, "   --pops -p (OPTIONAL) file mapping haplotype IDs to populations - \
+required if using -i and -v options\n");
+    fprintf(stderr, "   --include -i (OPTIONAL) require clades to include haplotypes \
+from a population given in the --pops file. Requires -p and -v options; may specify more than \
+once (use -i before each population name).\n");
     exit(code);
 }
 
@@ -159,8 +166,11 @@ int main(int argc, char *argv[]) {
        {"maxsize", required_argument, 0, 'S'},
        {"length", required_argument, 0, 'l'},
        {"freqs", required_argument, 0, 'f'},
-       {"p_cutoff", required_argument, 0, 'p'},
+       {"p_cutoff", required_argument, 0, 'P'},
        {"tmrca_cutoff", required_argument, 0, 't'},
+       {"indvs", required_argument, 0, 'v'},
+       {"pops", required_argument, 0, 'p'},
+       {"include", required_argument, 0, 'i'},
        {0, 0, 0, 0} 
     };
     
@@ -173,6 +183,11 @@ int main(int argc, char *argv[]) {
     bool freqs_given = false;
     float p_cutoff = 1.0;
     float tmrca_cutoff = 1.0;
+    string hapsfile;
+    bool hapsfile_given = false;
+    string popsfile;
+    bool popsfile_given = false;
+    vector<string> include_pops;
     
     int ch;
     /*
@@ -180,7 +195,7 @@ int main(int argc, char *argv[]) {
         help(0);
     }
     */
-    while((ch = getopt_long(argc, argv, "b:s:S:l:f:p:t:h", long_options, &option_index )) != -1){
+    while((ch = getopt_long(argc, argv, "b:s:S:l:f:P:t:v:p:i:h", long_options, &option_index )) != -1){
         switch(ch){
             case 0:
                 // This option set a flag. No need to do anything here.
@@ -201,11 +216,22 @@ int main(int argc, char *argv[]) {
                 freqsfile = optarg;
                 freqs_given = true;
                 break;
-            case 'p':
+            case 'P':
                 p_cutoff = atof(optarg);
                 break;
             case 't':
                 tmrca_cutoff = atof(optarg);
+                break;
+            case 'v':
+                hapsfile = optarg;
+                hapsfile_given = true;
+                break;
+            case 'p':
+                popsfile = optarg;
+                popsfile_given = true;
+                break;
+            case 'i':
+                include_pops.push_back((string)optarg);
                 break;
             case '?':
                 //help(0);
@@ -233,6 +259,12 @@ to 1\n");
     }
     if (tmrca_cutoff <= 0.0 || tmrca_cutoff > 1.0){
         fprintf(stderr, "ERROR: invalid TMRCA cutoff given.\n");
+        exit(1);
+    }
+    if (include_pops.size() > 0 && (!hapsfile_given || !popsfile_given)){
+        fprintf(stderr, "ERROR: you have specified one or more populations to require \
+in target clades, but have not specified the haplotype names file -v and/or have \
+not specified the haplotype/population mapping file -p.\n");
         exit(1);
     }
     
@@ -266,6 +298,51 @@ to 1\n");
         fprintf(stderr, "ERROR: maximum clade size must be <= %d\n", num_haplotypes-1);
         exit(1);
     }
+    vector<string> haps;
+    map<string, int> str2hap;
+    if (hapsfile_given){
+        parse_indvs(haps, hapsfile);
+        parse_indvs_map(str2hap, hapsfile);
+    }
+    
+    // Detect ploidy from haplotypes file
+    string firsthap = "";
+    int ploidy = 1;
+    for (int i = 0; i < haps.size(); ++i){
+        int last = haps[i].rfind("-");
+        string hap = haps[i];
+        if (last != string::npos){
+            hap = hap.substr(0, last);
+        }
+        if (firsthap == ""){
+            firsthap = hap;
+        }
+        else if (hap == firsthap){
+            ploidy++;
+        }
+        else{
+            break;
+        }
+    }
+    
+    map<string, string> indv2pop; 
+    map<string, vector<string> > pop2indv;
+    cladeset selected_haps;
+    bool use_selected_haps = false;
+    if (popsfile_given && hapsfile_given && include_pops.size() > 0){
+        parse_pops(indv2pop, pop2indv, popsfile, ploidy);
+        use_selected_haps = true;
+        // Now translate the selected populations into a bitset of 
+        // eligible haplotypes.
+        for (vector<string>::iterator pop = include_pops.begin(); 
+            pop != include_pops.end(); ++pop){
+            for (vector<string>::iterator hapname = pop2indv[*pop].begin();
+                hapname != pop2indv[*pop].end(); ++hapname){
+                int hapind = str2hap[*hapname];
+                selected_haps.set(num_haplotypes-1-hapind);
+            }   
+        }
+    }
     
     long int last_printed = 0;
     long int progress = 5000;
@@ -284,7 +361,9 @@ to 1\n");
         tree->set_haps(num_haplotypes);
         read_sitedata(num_haplotypes, is, chrom, pos, *tree);
         int ti = 0;
-        print_haplens(tree, ti, chrom, pos, minsize, maxsize, minlen, freqs_given, ages_freqs, p_cutoff, tmrca_cutoff);
+        print_haplens(tree, ti, chrom, pos, minsize, maxsize, minlen, 
+            freqs_given, ages_freqs, p_cutoff, tmrca_cutoff, use_selected_haps,
+            selected_haps);
         delete tree;
     }
     
